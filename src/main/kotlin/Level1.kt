de9adaf0ec3092fdfa11629bcf2eb7e27d0a09ce
Pia Gerhofer <pia.gerhofer@teamecho.at>
2021-11-05 16:34:23 +0100

    level 3

--- src/main/kotlin/Level1.kt
+++ src/main/kotlin/Level1.kt
@@ -1,58 +1,153 @@
 import java.io.File
+import java.lang.Error
+import java.util.*
+
+// TODO
+// skipping to end or start might be a problem if reserved words can be used as variable names or strings
 
 fun main(args: Array<String>) {
-    solveAll()
+   solveAll()
+    //val res = solveLevel1("C:\\Users\\pia\\Downloads\\untitled\\src\\main\\resources\\level3\\level3_example.in")
+    //println(res)
 }
 
-fun solveLevel1(filename: String): String {
-    var result = ""
+var index = 0
+var input = listOf<String>()
+var blocks : Deque<Block> = LinkedList()
+val variables = mutableMapOf<String, String>()
+const val error = "ERROR"
+
+data class Block(
+    val startIndex: Int,
+    val statement: String // start else or if
+)
+
+fun solveLevel1(filename: String): List<String> {
+    val results = mutableListOf<String>()
+    var currentResult = ""
     val lines = File(filename).readLines()
-    val allArgs = lines.drop(1)
-        .flatMap { it.split(" ") }
+    input = lines.drop(1)
+        .flatMap { it.trim().split(" ") }
+    index = 0
 
-    var index = 0
-    var returned = false
-
-    while (!returned && index < allArgs.size) {
-        val statement = allArgs[index]
-        when(statement) {
-            "print" -> {
-                result += allArgs[index+1]
-                index += 2
-            }
-            "start" -> index++
-            "end" -> index++
-            "return" -> returned = true
-            "if" -> {
-                val isTrue = allArgs[index + 1] == "true"
-                index += 2
-                if (!isTrue) {
-                    while (allArgs[index] != "else") {
+    while (index < input.size) {
+        try {
+            val statement = input[index]
+            when (statement) {
+                "var" -> {
+                    val name = input[index + 1]
+                    if (variables.containsKey(name)) {
+                        throw Error("Variable already exists")
+                    }
+                    val value = parseVariableOrValue(input[index + 2])
+                    variables[name] = value
+                    index += 3
+                }
+                "set" -> {
+                    val name = input[index + 1]
+                    if (!variables.containsKey(name)) {
+                        throw Error("Variable does not exist")
+                    }
+                    val value = parseVariableOrValue(input[index + 2])
+                    variables[name] = value
+                    index += 3
+                }
+                "print" -> {
+                    currentResult += parseVariableOrValue(input[index + 1])
+                    index += 2
+                }
+                "start" -> {
+                    variables.clear()
+                    blocks.clear()
+                    blocks.push(Block(index, "start"))
+                    index++
+                }
+                "end" -> {
+                    val currentBlock = blocks.pop()
+                    if (currentBlock.statement == "start") {
+                        results.add(currentResult)
+                        currentResult = ""
+                        index++
+                    } else if (currentBlock.statement == "if") {
+                        index += 2
+                        skipBlock()
+                    } else {
                         index++
                     }
+                }
+                "return" -> {
+                    results.add(currentResult)
+                    currentResult = ""
+                    moveToNextStart()
+                }
+                "if" -> {
+                    val ifIndex = index
+                    val isTrue = parseVariableOrBoolean(input[index + 1])
+                    index += 2
+                    if (!isTrue) {
+                        skipBlock()
+                    } else {
+                        blocks.push(Block(ifIndex, "if"))
+                    }
+                }
+                "else" -> {
+                    blocks.push(Block(index, "else"))
                     index++
                 }
-            }
-            "else" -> {
-                while (allArgs[index] != "end") {
-                    index++
-                }
-                index++
-            }
 
+            }
+        } catch (e: Error) {
+            currentResult = ""
+            results.add("ERROR")
+            moveToNextStart()
         }
     }
 
-    return result
+    return results
 }
 
+private fun moveToNextStart() {
+    while (input[index] != "start") {
+        index++
+    }
+}
+
+private fun skipBlock() {
+    var blockCounter = 1
+    while (blockCounter != 0) {
+        if (input[index] == "end") {
+            blockCounter -= 1
+        } else if (input[index] == "start" || input[index] == "if" || input[index] == "else") {
+            blockCounter += 1
+        }
+        index++
+    }
+}
+
+fun parseVariableOrBoolean(argument: String) : Boolean {
+    if (argument == "true" || argument == "false") {
+        return argument == "true"
+    } else {
+        val variable = variables[argument]
+        if (variable == "true") {
+            return true
+        } else if (variable == "false") {
+            return false
+        }
+    }
+    throw Error("can not parse boolean")
+}
+
+fun parseVariableOrValue(argument: String) : String {
+    return variables.getOrDefault(argument, argument)
+}
 
 fun solveAll() {
-    val level = "level2"
+    val level = "level3"
     val levelDir = "C:\\Users\\pia\\Downloads\\untitled\\src\\main\\resources\\$level"
 
     File(levelDir).walk().filter { it.isFile && it.absolutePath.endsWith(".in") }.forEach {
         val outPath = it.absolutePath.replace(".in", ".out")
-        File(outPath).writeText(solveLevel1(it.absolutePath))
+        File(outPath).writeText(solveLevel1(it.absolutePath).joinToString("\n"))
     }
 }
